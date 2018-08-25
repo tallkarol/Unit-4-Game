06a78bb2fba034a7fe60369d91712f15d3fdcd4d
tall karol <karolzbuczek@gmail.com>
2018-08-24 22:34:40 -0400

    finished v1

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,4 +1,5 @@
 // Initialize Variables
+window.onload = function() {
 
 var playerScore;
 var perfectScore;
@@ -11,42 +12,6 @@ var lossCount = 0;
 
 $("#totalWins").text("Total Wins: " + winCount)
 $("#totalLosses").text("Total Losses: " + lossCount)
-
-// Function to generate a number between a min and max
-
-var generateScore = function (min,max) {
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
-
-// Function to reset the scores (player, perfect, and each crystal) without a page reload
-
-var resetScores = function () {
-    playerScore = 0;
-    $("#currentScore").text("Current Score: " + playerScore);
-    perfectScore = generateScore(9,120);
-    $("#perfectScore").text("Perfect Score: " + perfectScore);
-    crystalOneScore = generateScore(1,12);
-    crystalTwoScore = generateScore(1,12);
-    crystalThreeScore = generateScore(1,12);
-    crystalFourScore = generateScore(1,12);
-}
-
-// Function to test if player wins or loses
-
-var runGame = function() {
-    if (playerScore === perfectScore) {
-        alert("Congratulations, you've achieved a balance");
-        winCount++;
-        $("#totalWins").text("Total Wins: " + winCount);
-        resetScores();
-    } else if (playerScore >= perfectScore) {
-        alert("Sorry, looks like you're not quite getting it.");
-        lossCount++;
-        $("#totalLosses").text("Total Losses: " + lossCount);
-        resetScores();
-    } else {
-    }
-}
 
 $("#crystalOne").click(function() {
     playerScore = playerScore + crystalOneScore;
@@ -71,8 +36,47 @@ $("#crystalFour").click(function() {
     $("#currentScore").text("Current Score: " + playerScore);
     runGame();
 });
+}
+
+// Function to generate a number between a min and max
+
+var generateScore = function (min,max) {
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+// Function to reset the scores (player, perfect, and each crystal) without a page reload
+
+var resetScores = function () {
+    playerScore = 0;
+    $("#currentScore").text("Current Score: " + playerScore);
+    perfectScore = generateScore(9,120);
+    $("#perfectScore").text("Perfect Score: " + perfectScore);
+    crystalOneScore = generateScore(1,12);
+    crystalTwoScore = generateScore(1,12);
+    crystalThreeScore = generateScore(1,12);
+    crystalFourScore = generateScore(1,12);
+}
+
+// Function to test if player wins or loses
+
+var runGame = function() {
+    resetScores()
+    if (playerScore === perfectScore) {
+        alert("Congratulations, you've achieved a balance");
+        winCount++;
+        $("#totalWins").text("Total Wins: " + winCount);
+        resetScores();
+    } else if (playerScore >= perfectScore) {
+        alert("Sorry, looks like you're not quite getting it.");
+        lossCount++;
+        $("#totalLosses").text("Total Losses: " + lossCount);
+        resetScores();
+    } else {
+    }
+}
+
+
 
 // Start the game
 
-resetScores()
 runGame()
